4d337f77e656a4d398b1cabb41da0901520a49dd
Chahat Gupta <chahat@hexoncode.com>
2021-12-29 15:16:09 +0530

    removed redundant imports

--- app/src/main/java/chtgupta/wallpaperapp/ui/activity/preview/PreviewActivity.kt
+++ app/src/main/java/chtgupta/wallpaperapp/ui/activity/preview/PreviewActivity.kt
@@ -3,47 +3,31 @@ package chtgupta.wallpaperapp.ui.activity.preview
 import android.annotation.SuppressLint
 import android.app.WallpaperManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import chtgupta.wallpaperapp.R
 import chtgupta.wallpaperapp.constant.EXTRA_WALLPAPER
 import chtgupta.wallpaperapp.data.Wallpaper
-import chtgupta.wallpaperapp.ui.activity.preview.PreviewActivityViewModel
 import chtgupta.wallpaperapp.ui.theme.WallpaperAppTheme
 import chtgupta.wallpaperapp.util.toast
-import com.airbnb.mvrx.MavericksState
-import com.airbnb.mvrx.MavericksViewModel
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.IOException
-import java.net.URL
 
 class PreviewActivity : ComponentActivity() {
 
